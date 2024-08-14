fc86804e34a9e2879af3d07aac5f68eb78696fdc
harsh17062000 <harshgupt100@gmail.com>
2024-08-14 13:31:56 +0530

    print two number

---
+++ accept_2no.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number1;
+    cout << "enter the number 1 " << "\n";
+    cin >> number1;
+
+    int number2;
+    cout << "enter the number 2 " << "\n ";
+    cin >> number2;
+
+    cout << "number 1 = " << number1 << "\n";
+    cout << "number 2 = " << number2 << "\n";
+
+    return 0;
+}
